6a002c57456fc11444306ce2993422611af2d63e
Donijustin <109790842+donijustin@users.noreply.github.com>
2022-07-27 22:53:03 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,47 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Dance Jewelry on Merchsummer</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Dance Jewelry on Merchsummer" />
         <p className="description">
-          only on <a href="https://www.merchsummer.com">Merchsummer</a> Get started by editing <code>pages/index.js</code>
+<a href="https://www.merchsummer.com">
+<img src="https://merchsummer.com/wp-content/uploads/2022/06/unisex-staple-t-shirt-black-heather-front-62ae87ecc62c7-300x300.jpg" alt="Merchsummer.com" width="100" height="132">
+</a>
+          Dance Jewelry 
+Dance jewelry is a popular accessory for dancers of all ages. Jewelry is typically not worn for dance classes since it can get in the way, catch on hair or clothing, and could possibly lead to injury. Dancers usually wear jewelry as a way to express themselves outside of dance class. They can choose jewelry items that show how much they love and appreciate the art form of dancing.
+
+<a href="https://www.merchsummer.com">Merchsummer</a>
+ Here for a Leading Dance Jewelry Resource!
+
+Dance Jewelry Options
+
+Dance jewelry usually has a symbol of a dancer performing a special maneuver such as a grand jeté leap. Many jewelry items use words that relate to dancing, such as jazz, tap, ballet, dreams, dance, and brand names of dance wear. Some necklace pendants are simply metal plates with words inscribed into them. Pendants can be worn on leather or metal necklace chains, with stainless steel being the most popular choice.
+
+A current jewelry trend involves using dance shoes or dance accessories as charms on necklaces and bracelets. For instance, a dancer who prefers the style of tap dancing may choose a miniature Tele Tone tap pendant on a necklace chain. Ballerinas may prefer to wear a necklace with pointe shoes or ballet slippers as a dangling pendant. Dancers who enjoy different styles of dance may want to purchase a charm bracelet and use charms that relate to each style of dance.
+
+Key rings are another popular type of dance jewelry. They can be purchased separately to keep your keys together, or to use as an accessory for dance bags. Some dancers will also choose to use key rings as pendants on necklace chains. Similar to other dance pendants, key rings can be inscribed with words or may use a picture of a dancer.
+
+Dance Leotards
+<a href="https://www.merchsummer.com/shop">
+<img src="https://merchsummer.com/wp-content/uploads/2022/06/merchsummer-photo-pageone-3.png" alt="Merchsummer.com/shop" width="100" height="132">
+</a>
+Dance leotards are most often worn for ballet, but they can also be worn for jazz, tap, lyrical jazz, and modern dance styles. Most ballet instructors require that students wear black leotards and pink tights for every class. This is necessary because ballet technique is very specific and ballet dancers should be uniform in their ballet performances. Ballet dancers can be original with the styles of black leotards that they choose. Leotard styles include tank, camisole, halter, criss cross, turtleneck, short sleeve, and long sleeve designs.
+
+<a href="https://www.merchsummer.com/shop">Merchsummer</a>
+ for a Leading Dance Leotards Resource!
+
+Dance leotards for types of dance other than ballet can be found in a variety of colors and materials. The most common materials for leotards are nylon, cotton, and Lycra. Materials such as microfiber polyester, spandex mesh, tricot, and velvet are also used for some leotard designs. Leotards may have one or many colors in matte, shiny, metallic, or printed designs.
+
+Brands of Dance Leotards
+
+Popular brands of dance leotards include Capezio, Eurotard, Mirella, Danskin, Bloch, and Body Wrappers. In addition to standard leotard designs, Capezio also features an ABT collection of specialty ballet leotards. Most ABT leotard styles feature striping that elongates the body and minimizes the waistline. Some styles also have a wrap front leg opening that elongates the leg line for ballerinas.
+
+Body Wrappers began as a company that made one size fits all leisure garments. Today, the company creates a variety of leotards in tank, camisole, double strap, long sleeve, halter, empire waist, darted, v-neck, and scoop neck styles in addition to other dance wear designs. Eurotard and Mirella are well-known for making leotards from soft, comfortable and durable fabrics.
+        
         </p>
       </main>
 
